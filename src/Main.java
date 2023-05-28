828c9a413eb8317eacc919fb44fd55c177867c3a
lili tolmacheva <tolmachevy2003@mail.ru>
2023-05-28 19:33:24 +0300

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int price = 10_000;
+
+        int bonus = price / 20;
+        System.out.println("Бонусы: " + bonus);
+
+    }
+
+}
